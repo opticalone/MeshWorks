5907b590b6e66e94edbc1a42423575d1784a3f76
Optical_1 <Optical_1@DESKTOP-PPBT747>
2018-09-18 08:30:13 -0700

    over night

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,15 @@ public class Driver : MonoBehaviour {
     public float maxSteerAngle = 45f;
     public float minSteerAngle = 25f;
     public float maxMotorTorque = 100f;
+    public float maxSpeed = 400f;
+    public float currentSpeed;
+    [Header("brakes")]
+    public Vector3 CenterOfMass;
+
+    public bool isBreaking = false;
+    public GameObject brakeLights;
+    //public Material brakeLightMat;
+    public float maxBrakTorque = 150f;
 
     [Header("wheels")]
     public Transform path;
@@ -26,12 +36,20 @@ public class Driver : MonoBehaviour {
     public WheelCollider br;
     public WheelCollider bl;
 
+    [Header("sensors")]
+
+    public float sensorLength = 10f;
+    public float sensorOffset = 1f;
+    public float sensorSideOffset = .5f;
+    public float sensorSideAngle = 35f;
+
     private List<Transform> nodes;
     private int currentNode = 0;
 
 	// Use this for initialization
 	void Start ()
     {
+        GetComponent<Rigidbody>().centerOfMass = CenterOfMass;
         Transform[] patrolTransforms = path.GetComponentsInChildren<Transform>();
         nodes = new List<Transform>();
         for (int i = 0; i < patrolTransforms.Length; i++)
@@ -39,23 +57,76 @@ public class Driver : MonoBehaviour {
             if (patrolTransforms[i] != path.transform)
             { nodes.Add(patrolTransforms[i]); }
         }
-        boost = Random.Range(motorBoost, maxMotorBoost);
-        switchDistance = Random.Range(maxSwitchDistance, minSwitchDistance);
+        boost = UnityEngine.Random.Range(motorBoost, maxMotorBoost);
+        switchDistance = UnityEngine.Random.Range(maxSwitchDistance, minSwitchDistance);
     }
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-       
+        Sense();
         ApplySteering();
+        ApplyBrakes();
         Drive();
         CheckWaypoint();
 	}
-    private void OnDrawGizmos()
+
+    private void Sense()
     {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(transform.position, nodes[currentNode].position);
+        RaycastHit hit;
+        Vector3 SensorStartPos = transform.position;
+        SensorStartPos.z += sensorOffset;
+        
+        if (Physics.Raycast(SensorStartPos,transform.forward, out hit,sensorLength))
+        {
+
+        }
+        Debug.DrawRay(SensorStartPos, hit.point);
+        SensorStartPos.x += sensorSideOffset;
+        if (Physics.Raycast(SensorStartPos, transform.forward, out hit, sensorLength))
+        {
+
+        }
+        Debug.DrawRay(SensorStartPos, hit.point);
+        if (Physics.Raycast(SensorStartPos, Quaternion.AngleAxis(sensorSideAngle, transform.up)*transform.forward, out hit, sensorLength))
+        {
+
+        }
+        Debug.DrawRay(SensorStartPos, hit.point);
+        SensorStartPos.x -= sensorSideOffset;
+        if (Physics.Raycast(SensorStartPos, transform.forward, out hit, sensorLength))
+        {
+
+        }
+        Debug.DrawRay(SensorStartPos, hit.point);
+        if (Physics.Raycast(SensorStartPos, Quaternion.AngleAxis(-sensorSideAngle, transform.up) * transform.forward, out hit, sensorLength))
+        {
+
+        }
+        Debug.DrawRay(SensorStartPos, hit.point);
     }
+
+    private void ApplyBrakes()
+    {
+        if (isBreaking)
+        {
+            br.brakeTorque = maxBrakTorque;
+            bl.brakeTorque = maxBrakTorque;
+            brakeLights.SetActive(true);
+        }
+        else
+        {
+            br.brakeTorque = 0;
+            bl.brakeTorque = 0;
+            brakeLights.SetActive(false);
+        }
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.blue;
+    //    Gizmos.DrawLine(transform.position, nodes[currentNode].position);
+    //}
 
 
 
@@ -73,10 +144,21 @@ public class Driver : MonoBehaviour {
 
     private void Drive()
     {
-        fl.motorTorque = (maxMotorTorque * boost); 
-        fr.motorTorque = (maxMotorTorque * boost); 
-        bl.motorTorque = (maxMotorTorque * boost);
-        br.motorTorque = (maxMotorTorque * boost); 
+        currentSpeed = 2 * Mathf.PI * fl.radius * fl.rpm * 60 / 1000;
+        if (currentSpeed < maxSpeed && !isBreaking)
+        {
+            fl.motorTorque = (boost);
+            fr.motorTorque = (boost);
+            bl.motorTorque = (maxMotorTorque * boost);
+            br.motorTorque = (maxMotorTorque * boost);
+        }
+        else
+        {
+            fl.motorTorque = 0;
+            fr.motorTorque = (0);
+            bl.motorTorque = (0);
+            br.motorTorque = (0);
+        }
     }
     private void CheckWaypoint()
     {
---
+++ Assets/Scripts/CarWheel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarWheel : MonoBehaviour {
+
+    public WheelCollider TargetWheelCollider;
+    private Vector3 wheelPos = new Vector3();
+    private Quaternion wheelRot = new Quaternion();
+	
+	// Update is called once per frame
+	void Update () {
+        TargetWheelCollider.GetWorldPose(out wheelPos, out wheelRot);
+        transform.position = wheelPos;
+        transform.rotation = wheelRot;
+	}
+}
